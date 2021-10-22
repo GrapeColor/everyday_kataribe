a89d1ed5623d9c9f7cf4183d352ab2e328332c4f
GrapeColor <grapecolor@users.noreply.github.com>
2021-10-23 02:55:43 +0900

    Igonore distribution

--- dist/environments.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ACTIVITY_STREAMING_URL = exports.ACTIVITY_STREAMING_NAME = exports.RANDOM_POST_TWEET_SCHEDULE = exports.TWITTER_BEARER_TOKEN = exports.EVERYDAY_KATARIBE_TOKEN = void 0;
-exports.EVERYDAY_KATARIBE_TOKEN = process.env['EVERYDAY_KATARIBE_TOKEN'] ?? '';
-exports.TWITTER_BEARER_TOKEN = process.env['TWITTER_BEARER_TOKEN'] ?? '';
-exports.RANDOM_POST_TWEET_SCHEDULE = process.env['RANDOM_POST_TWEET_SCHEDULE'] ?? '';
-exports.ACTIVITY_STREAMING_NAME = process.env['ACTIVITY_STREAMING_NAME'] ?? '';
-exports.ACTIVITY_STREAMING_URL = process.env['ACTIVITY_STREAMING_URL'] ?? '';
